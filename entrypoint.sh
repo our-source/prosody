c4b4931086910e2f239837f1e4c6a6d7390625dd
Johan Smits <johan@smitsmail.net>
2018-12-15 20:21:10 +0100

    Enable default storrage to sql and set archive expire to forever

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,6 +34,13 @@ http_upload_path = "/var/lib/prosody/http_upload";
 
 http_upload_file_size_limit = ${HTTP_FILE_UPLOAD_SIZE};
 
+# Store all data in the SQL backend
+default_storage = "sql"
+
+# MOM settings
+archive_expires_after = "never" -- forever
+
+
 EOF
 
 sed -i -e '/----------- Virtual hosts -----------/{r /tmp/http_config' -e 'd}' /etc/prosody/prosody.cfg.lua
